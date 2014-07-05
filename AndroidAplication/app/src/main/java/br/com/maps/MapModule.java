f167e4a341f58b3795771b4dc907066961cffa59
Fernando Marques de Souza Filho <fernando.marques@ice.ufjf.br>
2014-07-05 17:30:07 -0300

    Modulo de Camera implementado.

--- AndroidAplication/app/src/main/java/br/com/maps/MapModule.java
+++ AndroidAplication/app/src/main/java/br/com/maps/MapModule.java
@@ -1,10 +1,18 @@
 package br.com.maps;
 
+import android.location.Location;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 /**
  * Created by Fernando on 05/07/2014.
  */
 public class MapModule {
-    public void insertProblem(){
-
+    public static void insertProblem(Location location, GoogleMap mMap){
+          mMap.addMarker(new MarkerOptions()
+                  .position(new LatLng(location.getLatitude(), location.getLongitude()))
+                  .title("Hello world"));
     }
 }
--- AndroidAplication/app/src/main/java/br/com/maps/MapsActivity.java
+++ AndroidAplication/app/src/main/java/br/com/maps/MapsActivity.java
@@ -1,42 +1,55 @@
 package br.com.maps;
 
+import android.content.Intent;
 import android.location.Location;
-import android.support.v4.app.FragmentActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.Button;
 
-import com.google.android.gms.internal.gp;
-import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
-import android.widget.Button;
 
-
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 import br.com.denuncia.R;
 
-public class MapsActivity extends FragmentActivity  {
 
-    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+public class MapsActivity extends FragmentActivity  {
+    // Activity request codes
+    private Uri fileUri;
+
+    private static final String IMAGE_DIRECTORY_NAME = "DenunciaApp";
+
+    private static final int CAMERA_CAPTURE_IMAGE_REQUEST_CODE = 100;
+    private static final int CAMERA_CAPTURE_VIDEO_REQUEST_CODE = 200;
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    public static final int MEDIA_TYPE_VIDEO = 2;
+
+    protected GoogleMap mMap; // Might be null if Google Play services APK is not available.
 
     // GPSTracker class
     private GPSTracker gps;
-    Button btnShowLocation;
+    Button btnCapturePicture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Location location;
         setContentView(R.layout.activity_maps);
+
+        btnCapturePicture = (Button) findViewById(R.id.btnCapturePicture);
+
 
         mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
 
@@ -50,7 +63,29 @@ public class MapsActivity extends FragmentActivity  {
 
         mMap.moveCamera(center);
         mMap.animateCamera(zoom);
-        //setContentView(R.layout.activity_maps);
+        MapModule.insertProblem(location, mMap);
+
+
+        /**
+         * Capture image button click event
+         * */
+        btnCapturePicture.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // capture picture
+                captureImage();
+            }
+        });
+
+    }
+
+    public GoogleMap getmMap() {
+        return mMap;
+    }
+
+    public void setmMap(GoogleMap mMap) {
+        this.mMap = mMap;
     }
 
     @Override
@@ -63,8 +98,7 @@ public class MapsActivity extends FragmentActivity  {
 
     /**
      * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
-     * installed) and the map has not already been instantiated.. This will ensure that we only ever
-     * call {@link #setUpMap()} once when {@link #mMap} is not null.
+     * installed) and the map has not already been instantiated.
      * <p>
      * If it isn't installed {@link SupportMapFragment} (and
      * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
@@ -80,13 +114,62 @@ public class MapsActivity extends FragmentActivity  {
         mMap.setMyLocationEnabled(true);
     }
 
-    /**
-     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
-     * just add a marker near Africa.
-     * <p>
-     * This should only be called once and when we are sure that {@link #mMap} is not null.
+
+    /*
+     * Capturing Camera Image will lauch camera app requrest image capture
      */
-    private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+    private void captureImage() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+
+        fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+        // start the image capture Intent
+        startActivityForResult(intent, CAMERA_CAPTURE_IMAGE_REQUEST_CODE);
+    }
+    /*
+ * Creating file uri to store image/video
+ */
+    public Uri getOutputMediaFileUri(int type) {
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+
+    /*
+ * returning image / video
+ */
+    private static File getOutputMediaFile(int type) {
+
+        // External sdcard location
+        File mediaStorageDir = new File(
+                Environment
+                        .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                IMAGE_DIRECTORY_NAME);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d(IMAGE_DIRECTORY_NAME, "Oops! Failed create "
+                        + IMAGE_DIRECTORY_NAME + " directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",
+                Locale.getDefault()).format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator
+                    + "IMG_" + timeStamp + ".jpg");
+        } else if (type == MEDIA_TYPE_VIDEO) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator
+                    + "VID_" + timeStamp + ".mp4");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
     }
 }
+
