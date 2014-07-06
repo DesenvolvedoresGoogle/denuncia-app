a8c899b330da1b4b870cef178816afbb5c3f19af
Matheus Marques <matheus.marques@me.com>
2014-07-06 05:24:04 -0300

    Adicionado upload de imagem

--- DenunciaWeb/public/js/denuncia.map.js
+++ DenunciaWeb/public/js/denuncia.map.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 
 	initialLocation = null;
-	juizDeFora = new google.maps.LatLng(-21.7290588, -43.382521);
 	browserSupportFlag = new Boolean();
 	positionGlobal = {};
 	
@@ -16,12 +15,13 @@ $(document).ready(function() {
 	
 	 map = new google.maps.Map(document.getElementById("gmap"),
 			myOptions);
-
+	 markers = [];
+	 timeout = null;
+	 
 	// Try W3C Geolocation (Preferred)
 	if (navigator.geolocation) {
 		browserSupportFlag = true;
 		navigator.geolocation.getCurrentPosition(function(position) {
-			positionGlobal = position;
 			initialLocation = new google.maps.LatLng(position.coords.latitude,
 					position.coords.longitude);
 			map.setCenter(initialLocation);
@@ -39,19 +39,63 @@ $(document).ready(function() {
 		else
 			alert("Seu browser não suporta geolocalização.Nós te colocamos em Juiz de Fora.");
 
-		initialLocation = juizDeFora;
+		initialLocation = new google.maps.LatLng(-21.7290588, -43.382521);
 		map.setCenter(initialLocation);
 	}
 	
-	$.ajax({
-		type : 'POST',
-		url : 'http://denuncia.matheusmarques.com/api/get-near-reports/',
-		data : {'max': 50, 'latitude': positionGlobal.coords.latitude, 'longitude': positionGlobal.coords.longitude},
-		dataType : 'json'
-	}).done(function(data) {
-		
-	}).fail(function() {
-		alert("Erro ao carregar denúncias.");
-	});
+	google.maps.event.addListener(map, 'center_changed', function() {
+		deleteMarkers();
+		clearTimeout(timeout);
+		timeout = setTimeout(function() {
+	    	
+	      loadPoints(map.getCenter().lat(), map.getCenter().lng());
+	    }, 3000);
+	  });
+	
+	function loadPoints(latitude, longitude) {
+		$.ajax({
+			type : 'POST',
+			url : 'http://localhost/DenunciaApp/DenunciaWeb/public/api/get-near-reports/',
+			data : {'max': 50, 'latitude': latitude, 'longitude': longitude},
+			dataType : 'json'
+		}).done(function(data) {
+			$.each(data.reports, function(index, report) {
+				 
+	            var marker = new google.maps.Marker({
+	                position: new google.maps.LatLng(report.latitude, report.longitude),
+	                title: report.title,
+	                map: map
+	            });
+	            
+	            markers.push(marker);
+	 
+	        });
+		}).fail(function() {
+			alert("Erro ao carregar denúncias.");
+		});
+	}
+	
+	// Sets the map on all markers in the array.
+	function setAllMap(map) {
+	  for (var i = 0; i < markers.length; i++) {
+	    markers[i].setMap(map);
+	  }
+	}
+
+	// Removes the markers from the map, but keeps them in the array.
+	function clearMarkers() {
+	  setAllMap(null);
+	}
+
+	// Shows any markers currently in the array.
+	function showMarkers() {
+	  setAllMap(map);
+	}
+
+	// Deletes all markers in the array by removing references to them.
+	function deleteMarkers() {
+	  clearMarkers();
+	  markers = [];
+	}
 
 });
