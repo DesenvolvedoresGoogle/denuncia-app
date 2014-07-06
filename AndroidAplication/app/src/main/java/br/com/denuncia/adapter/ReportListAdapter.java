9481b8fce34716a414cc1bc087717299e35333f3
Jorge Moreira <jorge.smrr@gmail.com>
2014-07-05 21:42:59 -0300

    Lista para as denúncias

---
+++ AndroidAplication/app/src/main/java/br/com/denuncia/adapter/ReportListAdapter.java
@@ -0,0 +1,50 @@
+package br.com.denuncia.adapter;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
+import br.com.denuncia.R;
+
+/**
+ * Created by User on 05/07/2014.
+ */
+public class ReportListAdapter extends ArrayAdapter<Bitmap> {
+    private ArrayList<Bitmap> bitmaps;
+
+    public ReportListAdapter(Context context, ArrayList<Bitmap> bitmaps) {
+        super(context, R.layout.view_report, bitmaps);
+        this.bitmaps = bitmaps;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View row = convertView;
+        Holder holder = null;
+
+        if(row == null){
+            row = LayoutInflater.from(getContext()).inflate(R.layout.view_report, parent, false);
+            holder = new Holder();
+
+            holder.photo = ((ImageView) row.findViewById(R.id.view_report_photo));
+
+            row.setTag(holder);
+        } else {
+            holder = (Holder) row.getTag();
+        }
+
+        holder.photo.setImageBitmap(bitmaps.get(position));
+
+        return row;
+    }
+
+    private class Holder{
+        ImageView photo;
+    }
+}
